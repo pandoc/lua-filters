6e370edf0849ab27fcb3a55d9f046ad9f6d92a2a
Greg Tucker-Kellogg <gtuckerkellogg@gmail.com>
2018-01-23 16:18:28 +0800

    A filter for short captions in list of figures for latex output (#9)
    A filter for latex figure short captions using the figure attribute 'short-caption'

---
+++ short-captions/short-captions.lua
@@ -0,0 +1,28 @@
+
+if FORMAT == "latex" then
+   local function latex(str)
+      return pandoc.RawInline('latex', str)
+   end
+
+   function Para (figure)
+      if (#figure.content == 1) and (figure.content[1].t == 'Image')  and (figure.content[1].title == "fig:") then
+         local img = figure.content[1]
+         if img.caption and img.attributes['short-caption'] then
+            local caption_cmd = string.format('\n\\caption[%s]',img.attributes['short-caption'])
+            local hypertarget = latex("{%%\n")
+            local label = "\n"
+            if img.identifier ~= img.title then
+               hypertarget = string.format("\\hypertarget{%s}{%%\n",img.identifier)
+               label = string.format("\n\\label{%s}",img.identifier)
+            end
+            return pandoc.Para {
+               latex(hypertarget .. "\\begin{figure}\n\\centering\n"),
+               img,
+               latex(caption_cmd),
+               pandoc.Span(img.caption),
+               latex(label .."\n\\end{figure}\n}\n")
+            }
+         end
+      end
+   end
+end
