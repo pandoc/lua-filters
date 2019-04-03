b2babfe9ca4fd2f1fa890bf6475bf9c5b8ada0e1
Albert Krewinkel <albert@zeitkraut.de>
2019-04-03 07:41:53 +0200

    scrlttr2: replace golden file with test for expected strings
    Golden files tests are fragile, they break whenever the corresponding
    template is updated. Simply checking for the existence of certain
    elementary strings is sufficient and more robust.

---
+++ scrlttr2/expected-strings.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+latex_result="$(pandoc --to=latex --lua-filter=scrlttr2.lua -s sample.md)"
+
+assert_contains ()
+{
+    printf '%s' "$latex_result" | grep -qF "$1" -
+    if [ $? -ne 0 ]; then
+        printf 'Output does not contain `%s`.' "$1" >&2
+        exit 1
+    fi
+}
+
+# whether we are using the scrlttr2 class
+assert_contains '{scrlttr2}'
+
+assert_contains '\setkomavar{fromname}{Jane Doe}'
+assert_contains '\setkomavar{fromaddress}{35 Industry Way\\ Springfield}'
+assert_contains '\setkomavar{subject}{Letter of Reference}'
+assert_contains '\setkomavar{date}{February 29, 2020}'
+
+# Custom opening and default closing
+assert_contains '\opening{To Whom It May Concern,}'
+assert_contains '\closing{Sincerely,}'
+
+# Author and date
+assert_contains '\author{Jane Doe}'
+assert_contains '\date{February 29, 2020}'
+
+# Recipient address
+assert_contains '\begin{letter}{Fireworks Inc.\\ 123 Fake St\\ 58008 Springfield}'
