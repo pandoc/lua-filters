cf11cc340ac7944de40270aad340418af5e4ae56
Eli Flanagan <eflanagan@innovu.com>
2019-08-28 15:38:51 -0400

    revise syntax for simplicity

--- centerimage/centerimage.lua
+++ centerimage/centerimage.lua
@@ -1,8 +1,7 @@
 -- wrap images with a centering LaTeX snippet
 -- https://tex.stackexchange.com/questions/46903/centering-with-includegraphics-not-with-beginfigure
 function Image(elem)
-	local List = require 'pandoc.List'
-  return List:new{
+  return {
 		pandoc.RawInline('latex', '{\\centering'),
 		elem,
 		pandoc.RawInline('latex', '\\par}')
