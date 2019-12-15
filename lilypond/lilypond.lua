0283bb30736cc62e3f37ed96a799bea19437a1f9
Cole Miller <git@cole-miller.net>
2019-12-15 15:20:29 -0500

    Add some comments to the code

--- lilypond/lilypond.lua
+++ lilypond/lilypond.lua
@@ -22,10 +22,16 @@ local SPECIAL_ATTRIBUTES = {
         ["ly-title"] = true
       }
 
+-- pandoc.system.with_temporary_directory had a different (undocumented)
+-- name in the 2.7.3 release.
 local with_temporary_directory = tostring(PANDOC_VERSION) == "2.7.3"
                                    and pandoc.system.with_temp_directory
                                     or pandoc.system.with_temporary_directory
 
+-- This is the extra boilerplate that's added to code snippets when the
+-- `ly-fragment' class is present. It's adapted from what `lilypond-book'
+-- does. (The file `lilypond-book-preamble.ly' is placed on the include
+-- path as part of the default LilyPond installation.)
 local function wrap_fragment(src)
   return table.concat(
            {
@@ -94,6 +100,7 @@ local function process_lilypond(elem)
       return elem
     end
 
+    -- Are we dealing with an inline code element or a code block?
     local inline = elem.tag == "Code"
 
     local code = elem.text
@@ -112,9 +119,15 @@ local function process_lilypond(elem)
 
     local caption = elem.attributes["ly-caption"] or "Musical notation"
     local src = OPTIONS.relativize and make_relative_path(path, out_dir) or path
+    -- The "fig:" prefix causes this image to be rendered as a proper figure
+    -- in HTML ouput (this is a rather ugly pandoc feature and may be replaced
+    -- by something more elegant in the future).
     local fudge = inline and "" or "fig:"
+    -- Strip newlines, indendation, etc. from the code for a more readable title.
     local title = fudge .. (elem.attributes["ly-title"] or code:gsub("%s+", " "))
 
+    -- Strip most of the LilyPond-related attributes from this code element, for
+    -- tidiness.
     local classes = elem.classes:filter(
       function (cls)
         return not SPECIAL_CLASSES[cls]
@@ -122,6 +135,7 @@ local function process_lilypond(elem)
     )
     table.insert(
       classes,
+      -- Add one special class for styling/manipulation purposes.
       inline and "lilypond-image-inline"
               or "lilypond-image-standalone"
     )
@@ -137,6 +151,9 @@ local function process_lilypond(elem)
   end
 end
 
+-- Update `OPTIONS' based on the document metadata. There's a bit of
+-- munging because string options are parsed as (pandoc-flavored) Markdown
+-- and we have to dig out the raw Lua string.
 local function meta_transformer(md)
   local ly_block = md.lilypond or {}
 
@@ -154,10 +171,14 @@ local function code_transformer(elem)
   return process_lilypond(elem)
 end
 
+-- When replacing a block element we must wrap the generated image in
+-- a `Para' since `Image' is an inline element.
 local function code_block_transformer(elem)
   return pandoc.Para({process_lilypond(elem)})
 end
 
+-- Make sure the metadata transformation runs first so that the code
+-- transformations operate with the correct options.
 return {
          {Meta = meta_transformer},
          {Code = code_transformer, CodeBlock = code_block_transformer},
