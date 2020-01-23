cc24c9a68fee6eaf6fcb6bb1213a187e7bed6517
Huy <huynguyen.chem@berkeley.edu>
2020-01-23 14:32:05 -0600

    Fixed duplicate references

--- section-refs/section-refs.lua
+++ section-refs/section-refs.lua
@@ -6,10 +6,14 @@ function is_ref_header (blk)
    return (blk.t == "Header" and blk.identifier == "bibliography")
 end
 
+function is_first_header (blk)
+   return (blk.level == 1) -- identify if first header for resetting citations later
+end
+
 function get_all_refs (blks)
    for _, b in pairs(blks) do
       if is_ref_div(b) then
-	 return b.content
+         return b.content
       end
    end
 end
@@ -18,7 +22,7 @@ function remove_all_refs (blks)
    local out = {}
    for _, b in pairs(blks) do
       if not (is_ref_div(b) or is_ref_header(b)) then
-	 table.insert(out, b)
+         table.insert(out, b)
       end
    end
    return out
@@ -27,23 +31,41 @@ end
 -- We return a {number, ref} pair so we can sort in the individual
 -- bibliographies.
 function citation_to_numbered_ref (citation, all_refs)
-   local div_id = "ref-" .. citation.id
+   local div_id = "ref-" .. citation.id -- .. means concatenation
+   -- Iterate through the references and find the bib entry key that matches the citation key
+   -- Who would have know that ref-citation.id is the div_id for a reference
    for i, d in ipairs(all_refs) do
       if d.t == "Div" and d.identifier == div_id then
-	 return {i, d}
+         return {i, d} -- table of {citation index, ref-id}
+         -- Next, we need to find citation, which is an element with attributes id, mode,
+         -- prefix, suffix, note_num, hash
       end
    end
 end
 
+function table.contains(table, element)
+   for _, value in pairs(table) do
+     if value == element then
+       return true
+     end
+   end
+   return false
+ end
+
+-- function re_cite_note_num (blk)
+--    local cites = {}
+--    for k, c in pairs(blk.citations) do
+--       table.insert(cites, c)
 
 function get_partial_refs (blocks, all_refs)
+   -- We first find all the citations and store in the table 'cites'
    local cites = {}
    local citegetter = {
       Cite = function (el)
-	 for _, c in pairs(el.citations) do
-	    table.insert(cites, c)
-	 end
-      end
+               for _, c in pairs(el.citations) do
+                  table.insert(cites, c)
+               end
+            end
    }
 
    for _, b in pairs(blocks) do
@@ -58,15 +80,24 @@ function get_partial_refs (blocks, all_refs)
    for _, c in pairs(cites) do
       local r = citation_to_numbered_ref(c, all_refs)
       if r then
-	 table.insert(numbered_refs, r)
+         table.insert(numbered_refs, r)
       end
    end
 
    table.sort(numbered_refs, function(x, y) return x[1] < y[1] end)
+   
+   -- hack to reset citation counter
+   -- local first_ref_citation_number = numbered_refs[1][1]
+   -- local renumbered_refs = {}
+   -- for i, c in ipairs(numbered_refs) do
+   --    table.insert(renumbered_refs, i, c[2])
+   -- end
 
    local refs = {}
    for _, nr in pairs(numbered_refs) do
-      table.insert(refs, nr[2])
+      if not table.contains(refs, nr[2]) then
+         table.insert(refs, nr[2])
+      end
    end
 
    return refs
@@ -81,18 +112,18 @@ function add_section_refs (blks, lvl, refs_title, all_refs)
       refs_num = refs_num + 1
       local section_refs = get_partial_refs(section, all_refs)
       if refs_title then
-	 local hdr = pandoc.Header(lvl + 1,
+         local hdr = pandoc.Header(lvl + 1,
 				   refs_title,
 				   pandoc.Attr("bibliography-" .. tostring(refs_num),
 					       {"unnumbered"}))
-	 table.insert(section_refs, 1, hdr)
+         table.insert(section_refs, 1, hdr)
       end
       local refs_div = pandoc.Div(section_refs,
 				  pandoc.Attr("refs-" .. tostring(refs_num),
 					      {"references"}))
       table.insert(section, refs_div)
       for _, x in pairs(section) do
-	 table.insert(output_blks, x)
+         table.insert(output_blks, x)
       end
    end
 
@@ -100,15 +131,15 @@ function add_section_refs (blks, lvl, refs_title, all_refs)
    local seen_hdr_before = false
    for _, b in pairs(blks) do
       if b.t == "Header" and b.level <= lvl then
-	 if seen_hdr_before then
-	    go()
-	    section = {b}
-	 else
-	    seen_hdr_before = true
-	    table.insert(section, b)
-	 end
+         if seen_hdr_before then
+            go()
+            section = {b}
+         else
+            seen_hdr_before = true
+            table.insert(section, b)
+         end
       else
-	 table.insert(section, b)
+         table.insert(section, b)
       end
    end
    go()
