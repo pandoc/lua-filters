c6c7740276fce23135094637aad4505d33261840
Frederik Elwert <frederik.elwert@web.de>
2020-06-19 11:12:35 +0200

    Add character count (w/ and w/o spaces) to wordcount filter.

--- wordcount/README.md
+++ wordcount/README.md
@@ -1,11 +1,11 @@
 # wordcount
 
-This filter counts the words in the body of a document (omitting
+This filter counts the words and characters in the body of a document (omitting
 metadata like titles and abstracts), including words in code.
-It should be more accurate than `wc -w` run directly on a
-Markdown document, since the latter will count markup
+It should be more accurate than `wc -w` or `wc -m` run directly on a
+Markdown document, since `wc` will also count markup
 characters, like the `#` in front of an ATX header, or
-tags in HTML documents, as words.
+tags in HTML documents.
 
 To run it, `pandoc --lua-filter wordcount.lua myfile.md`.
 The word count will be printed to stdout.
--- wordcount/wordcount.lua
+++ wordcount/wordcount.lua
@@ -1,6 +1,8 @@
 -- counts words in a document 
 
 words = 0 
+characters = 0
+characters_and_spaces = 0
 
 wordcount = { 
   Str = function(el) 
@@ -8,16 +10,28 @@ wordcount = {
     if el.text:match("%P") then 
         words = words + 1 
     end 
+    characters = characters + utf8.len(el.text)
+    characters_and_spaces = characters_and_spaces + utf8.len(el.text)
   end, 
+
+  Space = function(el)
+    characters_and_spaces = characters_and_spaces + 1
+  end,
 
   Code = function(el) 
     _,n = el.text:gsub("%S+","") 
     words = words + n 
+    text_nospace = el.text:gsub("%s", "")
+    characters = characters + utf8.len(text_nospace)
+    characters_and_spaces = characters_and_spaces + utf8.len(el.text)
   end, 
 
   CodeBlock = function(el) 
     _,n = el.text:gsub("%S+","") 
     words = words + n 
+    text_nospace = el.text:gsub("%s", "")
+    characters = characters + utf8.len(text_nospace)
+    characters_and_spaces = characters_and_spaces + utf8.len(el.text)
   end 
 } 
 
@@ -25,5 +39,7 @@ function Pandoc(el)
     -- skip metadata, just count body: 
     pandoc.walk_block(pandoc.Div(el.blocks), wordcount) 
     print(words .. " words in body") 
+    print(characters .. " characters in body")
+    print(characters_and_spaces .. " characters in body (including spaces)")
     os.exit(0) 
 end
