c2e704ba384f279982e554c55e48386031233658
thomas.desaintexupery <3913267-thomas.desaintexupery@users.noreply.gitlab.com>
2020-07-20 10:23:31 +0200

    Add beautiful output for tests

--- runtests.sh
+++ runtests.sh
@@ -2,15 +2,28 @@
 # argument is list of filters
 
 FILTERS=$*
-let err=0
-for d in $FILTERS ; do
-    make --no-print-directory -C $d test
-    if [ $? -eq 0 ]; then
-    	echo "PASS $d"
-    else
-    	echo "FAIL $d"
-	err=1
-    fi
-done
-exit $err
 
+ERRORS_COUNT=0
+PASSED_COUNT=0
+SKIPPED_COUNT=0
+EXPECTED_COUNT=0
+
+let err=0
+
+for d in $FILTERS; do
+  let "EXPECTED_COUNT++"
+  make --no-print-directory -C $d test
+  if [ $? -eq 0 ]; then
+    let "PASSED_COUNT++"
+    echo "✓ $d"
+  else
+    let "ERRORS_COUNT++"
+    echo "✗ $d"
+  fi
+done
+
+echo ""
+echo "⚖ Summary"
+echo "✓ ${PASSED_COUNT} passed"
+[ ${ERRORS_COUNT} = 0 ] || echo "✗ ${ERRORS_COUNT} errors"
+exit ${ERRORS_COUNT}
