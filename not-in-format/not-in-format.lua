820e6974001ee2b0cd308527bff384891fbdac09
jdutant <34026710+jdutant@users.noreply.github.com>
2021-01-16 16:44:59 +0000

    improves code documentation
    The code comments are in LDoc format.

--- not-in-format/not-in-format.lua
+++ not-in-format/not-in-format.lua
@@ -1,14 +1,12 @@
---- not-in-format - keep document parts out of selected output formats.
+--- # not-in-format - keep document parts out of selected output formats.
 --
 -- This Lua filter for Pandoc that keeps parts of a document out of
 -- selected outputs formats.
 --
--- v1.0
---
 -- @author Julien Dutant <julien.dutant@kcl.ac.uk>
 -- @copyright 2021 Julien Dutant
 -- @license MIT - see LICENSE file for details.
---
+-- @release 1.0
 
 --- Test whether the target format is in a given list
 -- @param formats list of formats to be matched
@@ -22,9 +20,12 @@ local function format_matches(formats)
   return false
 end
 
---- Main Div filter
+--- Main Div filter.
 -- Removes Divs tagged with `not-in-format` if the
 -- output format matches one of the Div's attributes
+-- @param element a Div element
+-- @return an empty block if the Div classes include `not-in-format` and
+--  and the target format
 function Div(element)
   if element.classes:includes('not-in-format') and
     format_matches(element.classes) then
